35cffb2d9d567feb528f3d77e61ae3f53057cfec
sethu <sethupsanil666@gmail.com>
2023-06-11 20:41:04 +0530

    working on feature

--- src/components/feature/Feature.tsx
+++ src/components/feature/Feature.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-
-import './feature.css'
-const Feature = () => {
-  return (
-    <div>Feature</div>
-  )
+import "./feature.css";
+interface Props {
+  heading: string;
+  text: string;
 }
+const Feature = ({ heading, text }: Props) => {
+  return (
+    <div className="gpt3__features-container__feature">
+      <div className="gpt3__features-container__feature-title">
+        <div />
+        <h1>{heading}</h1>
+      </div>
+      <div className="gpt3__features-container_feature-text">
+        <p>{text}</p>
+      </div>
+    </div>
+  );
+};
 
-export default Feature
+export default Feature;
